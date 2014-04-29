c8483aded62d082c8d45815df9fd43c8548351a9
Geert Janssens <janssens-geert@telenet.be>
2014-04-29 09:54:37 +0200

    Fix invalid path assumptions in daily_build.sh

--- buildserver/daily_build.sh
+++ buildserver/daily_build.sh
@@ -18,9 +18,10 @@ function qpushd() { pushd "$@" >/dev/null; }
 function qpopd() { popd >/dev/null; }
 function unix_path() { echo "$*" | sed 's,^\([A-Za-z]\):,/\1,;s,\\,/,g'; }
 
-qpushd "$(dirname $(unix_path "$0"))"
-. ../functions.sh
-. ../defaults.sh
+_BUILDSERVER_UDIR="$(dirname $(unix_path "$0"))"
+qpushd "$_BUILDSERVER_UDIR/.."
+. ./functions.sh
+. ./defaults.sh
 
 # Variables
 _GIT_UDIR=`unix_path $GIT_DIR`
@@ -33,18 +34,16 @@ qpushd "$_GC_WIN_REPOS_UDIR"
 $GIT_CMD pull
 qpopd
 
-# Update the gnucash repository
-_REPOS_UDIR=`unix_path $REPOS_DIR`
-qpushd "$_REPOS_UDIR"
-$GIT_CMD pull
-qpopd
-
 ################################################################
 # determine if there are any new commits since the last time we ran
 #
+_REPOS_UDIR=`unix_path $REPOS_DIR`
+qpushd "$_REPOS_UDIR"
 
+# Update the gnucash repository
+$GIT_CMD pull
 # If we don't have a rev file then start from 'now' and force a build
-revfile=last_rev_daily
+revfile=$_GC_WIN_REPOS_UDIR/last_rev_daily
 if [ ! -f ${revfile} ] ; then
   echo $($GIT_CMD rev-parse HEAD) > ${revfile}
   oldrev=a   # definitely an invalid, so non-existing git rev
@@ -54,10 +53,11 @@ fi
 
 newrev=$($GIT_CMD rev-parse HEAD)
 if [[ "${oldrev}" != "${newrev}" ]]; then
-  ./build_package.sh
+  $_BUILDSERVER_UDIR/build_package.sh
 fi
 
 # move the new file into place, will only happen if the build was successful
 echo ${newrev} > ${revfile}
 
-qpopd
+qpopd # leave gnucash repository
+qpopd # return to directory the script was invoked from (not necessarily the directory this script resides in)
