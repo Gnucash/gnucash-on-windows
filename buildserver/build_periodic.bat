717c7db03faac6dad54aed1bbcb327c7199f409d
Geert Janssens <janssens-geert@telenet.be>
2014-05-21 16:20:37 +0200

    Enable weekly builds for maint now that maint and master have diverged

--- buildserver/build_periodic.bat
+++ buildserver/build_periodic.bat
@@ -11,4 +11,4 @@ c:\gcdev\mingw\msys\1.0\bin\sh.exe --login c:\gcdev\gnucash-on-windows.git\build
 rem Tags build for 2.6.99 and newer (daily -- only tags that weren't built yet)
 c:\gcdev\mingw\msys\1.0\bin\sh.exe --login c:\gcdev\gnucash-on-windows.git\buildserver\build_tags.sh
 rem maintenance branch build (weekly)
-rem c:\gcdev-maint\mingw\msys\1.0\bin\sh.exe --login c:\gcdev-maint\gnucash-on-windows.git\buildserver\periodic_build.sh weekly
+c:\gcdev-maint\mingw\msys\1.0\bin\sh.exe --login c:\gcdev-maint\gnucash-on-windows.git\buildserver\periodic_build.sh weekly
