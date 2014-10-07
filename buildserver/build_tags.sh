576134d4b637ec35f40608174d26119ab4f0c639
Geert Janssens <janssens-geert@telenet.be>
2014-10-07 17:36:36 +0200

    Build server: also mark deliberately skipped tags as built

--- buildserver/build_tags.sh
+++ buildserver/build_tags.sh
@@ -95,51 +95,52 @@ for tag_rev in $tags ; do
   if (( $major_minor < 205 ))
   then
      echo "Skipping build of tag $tag (reason: older than 2.5)"
-     continue
+  else
+
+    TAG_GLOBAL_DIR="c:\\gcdev\\gnucash-${tag}"
+    _TAG_GLOBAL_UDIR=$(unix_path "$TAG_GLOBAL_DIR")
+    rm -fr $_TAG_GLOBAL_UDIR
+
+    # Set up a clean build environment for this tag
+    # This will automatically create a custom.sh with
+    # several parameters correctly pre-set like
+    # GLOBAL_DIR, DOWNLOAD_DIR,...
+    cscript.exe $_GC_WIN_REPOS_UDIR/bootstrap_win_dev.vbs /silent:yes /GLOBAL_DIR:$TAG_GLOBAL_DIR /DOWNLOAD_DIR:$DOWNLOAD_DIR /GIT_DIR:$GIT_DIR
+
+    # Check out the tag and setup custom.sh
+    echo "Checking out tag $tag"
+    TAG_REPOS_DIR="${TAG_GLOBAL_DIR}\\gnucash.git"
+    _TAG_REPOS_UDIR=$(unix_path "$TAG_REPOS_DIR")
+    qpushd $TAG_REPOS_DIR
+      $GIT_CMD checkout $tag
+    qpopd
+
+    TAG_WIN_REPOS_DIR="${TAG_GLOBAL_DIR}\\gnucash-on-windows.git"
+    _TAG_WIN_REPOS_UDIR=$(unix_path "$TAG_WIN_REPOS_DIR")
+
+    # BUILD_FROM_TARBALL is special:
+    # in install.sh place we check !=yes, in defaults.sh =yes, in dist.sh =no
+    # We want it to look like 'no' in install and defaults, but yes in dist
+    # so this hack works!
+    echo "BUILD_FROM_TARBALL=maybe" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
+
+    # Point HH_DIR at the global installation because we don't need to redo it
+    echo -n "HH_DIR=" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
+    echo "${GLOBAL_DIR}\\hh" | sed -e 's/\\/\\\\/g' >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
+
+    # Inform the build scripts of the tag we're building.
+    echo "GNUCASH_SCM_REV=$tag" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
+
+    # Now build the tag!  (this will upload it too)
+    # Use the build_package script from master (cwd), not from the tag
+    qpushd ${_TAG_WIN_REPOS_UDIR}
+      ${BUILDSERVER_DIR}/build_package.sh ${tag}
+    qpopd
+  
   fi
 
-  TAG_GLOBAL_DIR="c:\\gcdev\\gnucash-${tag}"
-  _TAG_GLOBAL_UDIR=$(unix_path "$TAG_GLOBAL_DIR")
-  rm -fr $_TAG_GLOBAL_UDIR
-
-  # Set up a clean build environment for this tag
-  # This will automatically create a custom.sh with
-  # several parameters correctly pre-set like
-  # GLOBAL_DIR, DOWNLOAD_DIR,...
-  cscript.exe $_GC_WIN_REPOS_UDIR/bootstrap_win_dev.vbs /silent:yes /GLOBAL_DIR:$TAG_GLOBAL_DIR /DOWNLOAD_DIR:$DOWNLOAD_DIR /GIT_DIR:$GIT_DIR
-
-  # Check out the tag and setup custom.sh
-  echo "Checking out tag $tag"
-  TAG_REPOS_DIR="${TAG_GLOBAL_DIR}\\gnucash.git"
-  _TAG_REPOS_UDIR=$(unix_path "$TAG_REPOS_DIR")
-  qpushd $TAG_REPOS_DIR
-  $GIT_CMD checkout $tag
-  qpopd
-
-  TAG_WIN_REPOS_DIR="${TAG_GLOBAL_DIR}\\gnucash-on-windows.git"
-  _TAG_WIN_REPOS_UDIR=$(unix_path "$TAG_WIN_REPOS_DIR")
-
-  # BUILD_FROM_TARBALL is special:
-  # in install.sh place we check !=yes, in defaults.sh =yes, in dist.sh =no
-  # We want it to look like 'no' in install and defaults, but yes in dist
-  # so this hack works!
-  echo "BUILD_FROM_TARBALL=maybe" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
-
-  # Point HH_DIR at the global installation because we don't need to redo it
-  echo -n "HH_DIR=" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
-  echo "${GLOBAL_DIR}\\hh" | sed -e 's/\\/\\\\/g' >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
-
-  # Inform the build scripts of the tag we're building.
-  echo "GNUCASH_SCM_REV=$tag" >> ${_TAG_WIN_REPOS_UDIR}/custom.sh
-
-  # Now build the tag!  (this will upload it too)
-  # Use the build_package script from master (cwd), not from the tag
-  qpushd ${_TAG_WIN_REPOS_UDIR}
-    ${BUILDSERVER_DIR}/build_package.sh ${tag}
-  qpopd
-  
   # Successful build of one tag. We may be in a loop to build several tags.
-  # So mark this one as done to prevent it from being rebuilt if a subsequent
+  # So mark this one as done to prevent it from being restarted if a subsequent
   # build fails.
   built_tags="${built_tags}${tag_rev}"$'\n'
   echo "${built_tags}" | sort  | grep -v '^$' > ${tagfile}
