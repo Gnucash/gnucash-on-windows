ae8a391a89edcb78b7cfc44e1adb4ca7be10de16
Geert Janssens <janssens-geert@telenet.be>
2015-05-22 15:49:49 +0200

    Fix filename references after Frank's global rename of scm to vcs

--- dist-impl.sh
+++ dist-impl.sh
@@ -278,7 +278,7 @@ function dist_finish() {
     if [ "$BUILD_FROM_TARBALL" = "no" ]; then
         # And changing output filename
         PKG_VERSION=`grep PACKAGE_VERSION ${_BUILD_UDIR}/config.h | cut -d" " -f3 | cut -d\" -f2 `
-        REVISION=`grep GNUCASH_SCM_REV ${_BUILD_UDIR}/src/core-utils/gnc-scm-info.h | cut -d" " -f3 | cut -d\" -f2 `
+        REVISION=`grep GNUCASH_SCM_REV ${_BUILD_UDIR}/src/core-utils/gnc-vcs-info.h | cut -d" " -f3 | cut -d\" -f2 `
         SETUP_FILENAME="gnucash-${PKG_VERSION}-$(date +'%Y-%m-%d')-${REPOS_TYPE}-${REVISION}-setup.exe"
         qpushd ${_GNUCASH_UDIR}
             mv gnucash-${PKG_VERSION}-setup.exe ${SETUP_FILENAME}
