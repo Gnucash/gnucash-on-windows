19cdac2741e81bc7007c271d1a7f33872a30f85f
John Ralls <jralls@ceridwen.us>
2020-11-20 16:43:53 -0800

    Update MSYS2 installers to latest versions.

--- setup-mingw64.ps1
+++ setup-mingw64.ps1
@@ -139,8 +139,8 @@ function make-unixpath([string]$path) {
 # Install MSYS2 for the current machine's architechture.
 
 if (!(test-path -path $bash_path)) {
-    $mingw64_installer32 = "$preferred_mirror/distrib/i686/msys2-i686-20190524.exe"
-    $mingw64_installer64 = "$preferred_mirror/distrib/x86_64/msys2-x86_64-20190524.exe"
+    $mingw64_installer32 = "$preferred_mirror/distrib/i686/msys2-i686-20200903.exe"
+    $mingw64_installer64 = "$preferred_mirror/distrib/x86_64/msys2-x86_64-20200903.exe"
 
     $mingw64_installer = If ([IntPtr]::size -eq 4) {$mingw64_installer32} Else {$mingw64_installer64}
     $msys_install_dir = (join-path $target_dir "msys2") -replace "\\", '/'
